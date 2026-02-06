98d0ee023aa0c7478b2b62424ce24f46523d6518
zombi-filin <zombi.filin.256@gmail.com>
2026-02-06 06:55:49 +0300

    Дополнение

--- src/DS3231.h
+++ src/DS3231.h
@@ -1,13 +1,13 @@
 #ifndef __DS3231_H__
 #define __DS3231_H__
 
-#include <Wire.h>
+#include <stdint.h>
 
 #define DS3231_I2C_ADDRESS  0x68
 
 struct DS3231_datetime_t
 {
-    uint16_t year;      // Год
+    uint8_t year;       // Год
     uint8_t month;      // Месяц
     uint8_t day_month;  // День 1-31
     uint8_t day_week;   // День недели 1=Воскресенье, 7=Суббота
@@ -15,6 +15,7 @@ struct DS3231_datetime_t
     uint8_t minute;     // Минуты
     uint8_t second;     // Секунды
 };
+
 
 
 #endif // __DS3231_H__
--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,21 @@
 #define BLINK_DELAY   500
 
 void setup() {
-  // Инициализация I2C
-  Wire.begin(PIN_WIRE_SDA, PIN_WIRE_SCL);
+	// Инициализация I2C
+	Wire.begin(PIN_WIRE_SDA, PIN_WIRE_SCL);
 
-  // Настройка порта монитора
-  Serial.begin(74880);
+	// Настройка порта монитора
+	Serial.begin(74880);
   
-  //
-  pinMode(LED_PIN, OUTPUT);
+	//
+	pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH);
-  delay(BLINK_DELAY);
-  digitalWrite(LED_PIN,LOW);
-  delay(BLINK_DELAY);
-  
-  Serial.println("Loop");
+	digitalWrite(LED_PIN, HIGH);
+	delay(BLINK_DELAY);
+	digitalWrite(LED_PIN,LOW);
+	delay(BLINK_DELAY);
+
+	Serial.println("Loop");
 }
